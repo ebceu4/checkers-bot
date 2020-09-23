5433db2e0314f6e6ec113bfe2234270f6a7e92ca
Yuriy Naydenov <ebceu44@gmail.com>
2020-09-23 13:32:42 +0300

    *

--- src/api/info.ts
+++ src/api/info.ts
@@ -5,6 +5,6 @@ import Telegraf from 'telegraf'
 export default async (req: NowRequest, res: NowResponse) => {
   console.log(process.env)
   const bot = new Telegraf(process.env.BOT_TOKEN!)
-  const r = await bot.telegram.setWebhook(`https://checkersgamebot.vercel.app/api/bot/${process.env.BOT_TOKEN!}`)
-  res.send(r)
+  const info = await bot.telegram.getWebhookInfo()
+  res.send(info)
 }
