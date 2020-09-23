156738dffdb921f01021565aa860d35cd94ea7d2
Yuriy Naydenov <ebceu44@gmail.com>
2020-09-23 14:26:33 +0300

    *

--- src/api/bot/[token]/index.ts
+++ src/api/bot/[token]/index.ts
@@ -2,17 +2,20 @@ import Telegraf from 'telegraf'
 import { NowRequest, NowResponse } from '@vercel/node'
 import { withAuth } from '../withAuth'
 
-const telegraf = new Telegraf(process.env.BOT_TOKEN!)
-telegraf.use(Telegraf.log())
 
-telegraf.start((ctx) => ctx.reply('Welcome'))
-telegraf.help((ctx) => ctx.reply('Send me a sticker'))
-telegraf.on('sticker', (ctx) => ctx.reply('👍'))
-telegraf.hears('hi', (ctx) => ctx.reply('Hey there'))
-telegraf.launch({ webhook: { hookPath: `/api/bot/${process.env.BOT_TOKEN!}`, domain: 'https://checkersgamebot.vercel.app' } })
 
 export default withAuth(async (req: NowRequest, res: NowResponse) => {
   console.log('BODY', req.body)
+
+  const telegraf = new Telegraf(process.env.BOT_TOKEN!)
+  telegraf.use(Telegraf.log())
+
+  telegraf.start((ctx) => ctx.reply('Welcome'))
+  telegraf.help((ctx) => ctx.reply('Send me a sticker'))
+  telegraf.on('sticker', (ctx) => ctx.reply('👍'))
+  telegraf.hears('hi', (ctx) => ctx.reply('Hey there'))
+  await telegraf.launch()
+
   const result = await telegraf.handleUpdate(req.body, res)
   console.log('Result', result)
 })
