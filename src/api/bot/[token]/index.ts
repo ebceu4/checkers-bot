597d5b790fb3e254ba4dd71c7a949c1259811060
Yuriy Naydenov <ebceu44@gmail.com>
2020-09-23 13:58:03 +0300

    *

--- src/api/bot/[token]/index.ts
+++ src/api/bot/[token]/index.ts
@@ -3,10 +3,14 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import { withAuth } from '../withAuth'
 
 const telegraf = new Telegraf(process.env.BOT_TOKEN!)
+telegraf.use(Telegraf.log())
+
 telegraf.start((ctx) => ctx.reply('Welcome'))
 telegraf.help((ctx) => ctx.reply('Send me a sticker'))
 telegraf.on('sticker', (ctx) => ctx.reply('👍'))
 telegraf.hears('hi', (ctx) => ctx.reply('Hey there'))
+telegraf.launch()
+
 
 export default withAuth(async (req: NowRequest, res: NowResponse) => {
   console.log('BODY', req.body)
