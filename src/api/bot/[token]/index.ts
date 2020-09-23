b5ed2a41a313301a7e7f624064299d18bf982017
Yuriy Naydenov <ebceu44@gmail.com>
2020-09-23 19:51:48 +0300

    *

--- src/api/bot/[token]/index.ts
+++ src/api/bot/[token]/index.ts
@@ -3,35 +3,16 @@ import { withTelegramHook } from '../withTelegram'
 
 export default withAuth(withTelegramHook(async (telergam, update) => {
   console.log(update)
-  telergam.sendMessage(update.message?.chat?.id!, `echo: ${update.message?.text}`)
+
+  const chatId = update.message?.chat?.id!
+  if (!chatId)
+    return
+
+  if (update.message?.sticker) {
+    telergam.sendSticker(chatId, update.message.sticker.file_id)
+  }
+  else {
+    telergam.sendMessage(chatId, `echo: ${update.message?.text}`)
+  }
+
 }))
-
-// export interface Update {
-//   update_id: number
-//   message: Message
-// }
-
-// export interface Message {
-//   message_id: number
-//   from: From
-//   chat: Chat
-//   date: number
-//   text: string
-// }
-
-// export interface Chat {
-//   id: number
-//   first_name: string
-//   last_name: string
-//   username: string
-//   type: string
-// }
-
-// export interface From {
-//   id: number
-//   is_bot: boolean
-//   first_name: string
-//   last_name: string
-//   username: string
-//   language_code: string
-// }
